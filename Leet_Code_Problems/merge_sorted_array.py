8acf6411494f397c7e70bb627a5223849a291fe5
Akshay-Gudekar <akshaygudekar01@gmail.com>
2026-01-12 12:30:07 +0100

    Added merge_sorted_array.py

---
+++ Leet_Code_Problems/merge_sorted_array.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        p1, p2, p = m - 1, n - 1, m + n - 1
+
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+
+        nums1[:p2 + 1] = nums2[:p2 + 1]
+
+
+
+if __name__ == "__main__":
+    n1 = [1, 2, 3, 0, 0, 0]
+    m_val = 3
+    n2 = [2, 5, 6]
+    n_val = 3
+
+    sol = Solution()
+    sol.merge(n1, m_val, n2, n_val)
+
+
+    print(f"Merged Array: {n1}")
